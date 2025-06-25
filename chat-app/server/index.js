31137ed857d799f87dd5e1b96b248489e902b8b8
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-06-25 12:31:33 +0700

    Basic Websocket

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -1,9 +1,10 @@
 const ws = require('ws') //install websocket
-const server = new ws.server({port: '3000'}) //define server
+const server = new ws.Server({port: '3000'}) //define server
 
 server.on('connection', socket => {
     socket.on('message', message => {
-        console.log(message)
+        const b = Buffer.from(message)
+        console.log(b.toString())
         socket.send(`${message}`)
     })
 })
