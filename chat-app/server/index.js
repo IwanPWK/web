ec35e0ef914f57774e3dbc0bb1e0a2125eb08836
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-06-26 11:43:29 +0700

    Chat Activity Detection

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -1,33 +1,54 @@
-// use import not require, coz using module type
-import express from 'express'
-import { Server } from "socket.io"
-import path from 'path' // setup static or frontend on the same server as the backend (host frontend with express)
-import { fileURLToPath } from 'url' // fix __dirname is unknown coz using import
+// 3 use import not require, coz using module type
+import express from 'express' // 3
+import { Server } from "socket.io" // 3
+import path from 'path' // 3 setup static or frontend on the same server as the backend (host frontend with express)
+import { fileURLToPath } from 'url' // 3 fix __dirname is unknown coz using import
 
-const __filename = fileURLToPath(import.meta.url) // fix __dirname is unknown coz using import
-const __dirname = path.dirname(__filename) // fix __dirname is unknown coz using import
+const __filename = fileURLToPath(import.meta.url) // 3 fix __dirname is unknown coz using import
+const __dirname = path.dirname(__filename) // 3 fix __dirname is unknown coz using import
 
 
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 3500 // 3
 
-const app = express()
+const app = express() // 3
 
-app.use(express.static(path.join(__dirname, "public"))) // define static folder
+app.use(express.static(path.join(__dirname, "public"))) // 3 define static folder
 
-const expressServer = app.listen(PORT, ()=>{
+// 3
+const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
 
-const io = new Server(expressServer, {
-    cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"] // not necessary if frontend and backend are both on the same server
-    }
-})
+const io = new Server(
+    expressServer, // 3
+    {
+        cors: {
+            origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"] // not necessary if frontend and backend are both on the same server
+        }
+    })
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
+
+    // 4 Upon connection - only to user, when a user connects
+    socket.emit('message', "Welcome to Chat App!")
+
+    // 4 Upon connection - to all others (except the user)
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`)
+
+    // 4 Listening for a message event
     socket.on('message', data => {
         console.log(data)
-        io.emit('message',`${socket.id.substring(0,5)}: ${data}`)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    // 4 When user disconnects - to all others
+    socket.on('disconnect', ()=> {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`)
+    })
+
+    // 4 Listen for activity
+    socket.on('activity', name => {
+        socket.broadcast.emit('activity', name)
     })
 })
--- chat-app/server/public/app.js
+++ chat-app/server/public/app.js
@@ -1,20 +1,42 @@
-const socket =  io("ws://localhost:3500"); 
 
+const socket = io("ws://localhost:3500");  // 2
+
+const activity = document.querySelector('.activity') // 4
+const msgInput = document.querySelector("input"); // 4
+
+// 4
 function sendMessage(e) {
   e.preventDefault(); //submit the form without reloading the page
-  const input = document.querySelector("input");
-  if (input.value) {
-    socket.emit('message', input.value);
-    input.value = "";
+  if (msgInput.value) {
+    socket.emit('message', msgInput.value);
+    msgInput.value = "";
   }
-  input.focus();
+  msgInput.focus();
 }
 
-document.querySelector("form").addEventListener("submit", sendMessage);
+document.querySelector("form").addEventListener("submit", sendMessage); // 2
 
-// Listen for messages
-socket.on("message", (data ) => {
+// 2 Listen for messages
+socket.on("message", (data) => {
+  activity.textContent = ""
   const li = document.createElement("li");
   li.textContent = data;
   document.querySelector("ul").appendChild(li);
 });
+
+// 4
+msgInput.addEventListener('keypress', () => {
+  socket.emit('activity', socket.id.substring(0, 5)) // 4 first five char to identify the current user
+})
+
+// 4
+let activityTimer
+socket.on("activity", name => {
+  activity.textContent = `${name} is typing...`
+
+  //Clear after 3 seconds
+  clearTimeout(activityTimer)
+  activityTimer = setTimeout(()=> {
+    activity.textContent = ""
+  }, 3000)
+})
