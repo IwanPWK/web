c7c0839132c9096a14c4b54b309139f751283523
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-06-26 10:24:00 +0700

    Setup Frontend and Backend, both are on the same server

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -1,11 +1,26 @@
-import { createServer } from "http"
+// use import not require, coz using module type
+import express from 'express'
 import { Server } from "socket.io"
+import path from 'path' // setup static or frontend on the same server as the backend (host frontend with express)
+import { fileURLToPath } from 'url' // fix __dirname is unknown coz using import
 
-const httpServer = createServer()
+const __filename = fileURLToPath(import.meta.url) // fix __dirname is unknown coz using import
+const __dirname = path.dirname(__filename) // fix __dirname is unknown coz using import
 
-const io = new Server(httpServer, {
+
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public"))) // define static folder
+
+const expressServer = app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
+
+const io = new Server(expressServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5501", "http://127.0.0.1:5501"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"] // not necessary if frontend and backend are both on the same server
     }
 })
 
@@ -16,5 +31,3 @@ io.on('connection', socket => {
         io.emit('message',`${socket.id.substring(0,5)}: ${data}`)
     })
 })
-
-httpServer.listen(3500, ()=> console.log('listening on port 3500'))
